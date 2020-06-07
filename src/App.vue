c35e6d8b3b4cf9ac3073f78cf7a2935c0137a176
yubachiri <k.i.moso9532@gmail.com>
2020-06-07 23:46:23 +0900

    実装完了

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <label for="name-input">氏名</label>
         <input
           type="text"
-          v-model="state.name"
+          v-model="form.name.$value"
           id="name-input"
         />
       </div>
@@ -14,7 +14,7 @@
         <label for="age-input">年齢</label>
         <input
           type="number"
-          v-model="state.age"
+          v-model="form.age.$value"
           id="age-input"
         />
       </div>
@@ -31,7 +31,7 @@
       <div class="mt">
         <label for="profile-input">プロフィール</label>
         <textarea
-          v-model="state.profile"
+          v-model="form.profile.$value"
           rows="4"
           cols="40"
           id="profile-input"
@@ -39,10 +39,10 @@
       </div>
 
       <div class="mt">
-        <label for="tel-input">電話番号</label>
+        <label for="tel-input">電話番号(ハイフン含)</label>
         <input
           type="text"
-          v-model="state.telNumber"
+          v-model="form.telNumber.$value"
           id="tel-input"
         />
       </div>
@@ -51,6 +51,7 @@
         <label for="address-input">都道府県</label>
         <select
           v-model="state.address"
+          id="address-input"
         >
           <option>都</option>
           <option>道</option>
@@ -60,7 +61,18 @@
       </div>
 
       <div class="mt">
-        <input type="submit"/>
+        <input
+          :disabled="form.$anyInvalid"
+          type="submit"
+        />
+      </div>
+
+      <div>
+        <p v-if="form.name.$anyInvalid">氏名: {{form.name.$message}}</p>
+        <p v-if="form.age.$anyInvalid">年齢: {{form.age.$message}}</p>
+        <p v-if="form.birthday.$anyInvalid">生年月日: {{form.birthday.$message}}</p>
+        <p v-if="form.profile.$anyInvalid">プロフィール: {{form.profile.$message}}</p>
+        <p v-if="form.telNumber.$anyInvalid">電話番号: {{form.telNumber.required.$message}} {{form.telNumber.format.$message}}</p>
       </div>
     </form>
   </div>
@@ -68,6 +80,7 @@
 
 <script lang="ts">
 import {defineComponent, reactive} from '@vue/composition-api'
+import {useValidation} from 'vue-composable'
 
 type State = {
   name: string;
@@ -78,10 +91,18 @@ type State = {
   address: string;
 }
 
+const required = x => !!x
+const numerically = v => {
+  return v.match(/\d+/)
+};
+const telNumberFormat = v => {
+  return v.match(/\d{2,3}-\d{1,4}-\d{4}$/)
+}
+
 export default defineComponent({
   setup() {
     const state = reactive<State>({
-      name: "name",
+      name: "",
       age: 20,
       birthDay: new Date(),
       profile: "",
@@ -89,12 +110,47 @@ export default defineComponent({
       address: "都"
     });
 
+    const form = useValidation({
+      name: {
+        $value: state.name,
+        required,
+        $message: "必須項目です"
+      },
+      age: {
+        $value: state.age,
+        numerically,
+        $message: "数値を入力してください"
+      },
+      birthday: {
+        $value: state.birthDay,
+        required,
+        $message: "必須項目です"
+      },
+      profile: {
+        $value: state.profile,
+        required,
+        $message: "必須項目です"
+      },
+      telNumber: {
+        $value: state.telNumber,
+        required: {
+          $validator: required,
+          $message: "必須項目です"
+        },
+        format: {
+          $validator: telNumberFormat,
+          $message: "電話番号の形式が不正です"
+        }
+      }
+    })
+
     function submit() {
       alert('called submit')
     }
 
     return {
       state,
+      form,
       submit
     }
   }
